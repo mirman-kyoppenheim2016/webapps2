b4fa787790822cd169b13278692e49042f38241a
mirman-kyoppenheim2016 <kyoppenheim2016@mirmanmustangs.org>
2015-10-22 15:03:24 -0700

    October 22nd Push

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -10,9 +10,8 @@ var xPlanes = [
 
 // Now that we have our array of planes, we need to add elements to our DOM
 // for each one.
-var e = document. //This line is unfinished! Use a method to capture our <ul> element.
-
+var e = document.getElementById //This line is unfinished! Use a method to capture our <ul> element.
 for(var i=0; i<xPlanes.length; i++) {
   var xPlane = xPlanes[i]; //What does "i" refer to?
-  // Do some things to add <li> elements to our empty list.
+append('<li>'+xPlanes+'</li>')// Do some things to add <li> elements to our empty list.
 }
